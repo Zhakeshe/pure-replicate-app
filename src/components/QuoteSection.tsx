51f15a92456b8613ef9062401f6d9d1c5a73f67e
Zhakeshe <zhakezh3@gmail.com>
2025-12-30 12:15:42 +0600

    Update Instagram link for quote section

--- src/components/QuoteSection.tsx
+++ src/components/QuoteSection.tsx
@@ -34,7 +34,7 @@ const QuoteSection = () => {
             </div>
 
             <a
-              href="https://www.instagram.com/"
+              href="https://www.instagram.com/kulanman?igsh=MWRmdTVzZGR1Y21ubg=="
               target="_blank"
               rel="noreferrer"
               className="inline-flex items-center justify-center px-5 py-3 rounded-xl bg-[#8a0f27] text-white font-semibold shadow-[0_16px_40px_rgba(138,15,39,0.35)] hover:shadow-[0_22px_60px_rgba(138,15,39,0.4)] hover:scale-[1.02] transition-all duration-200"

7c66e8de585e1c862ad7d49c006396ae6d9912f2
Zhakeshe <zhakezh3@gmail.com>
2025-12-30 12:46:18 +0600

    Update speaker image to new URL

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -8,7 +8,7 @@ export const SPEAKER_BACKGROUND_URL =
   "https://static.tildacdn.com/tild6233-6662-4661-b730-373863663463/-min.png";
 
 export const SPEAKER_IMAGE_URL =
-  "https://fotohosting.pro/i/1W6A6693-resized-%281%29-no-bg-HD-%28carve.photos%29.kxdXX";
+  "https://i.postimg.cc/T1t02HN9/1W6A6693-resized-(1)-no-bg-HD-(carve-photos).png";
 
 export const SPEAKER_IMAGE_FALLBACK_URL =
   "https://static.tildacdn.com/tild6438-3266-4131-b336-336434313833/Mask_group-min.png";
